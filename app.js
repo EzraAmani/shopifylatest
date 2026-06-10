b2be05e4d3213ae9f25ead8c1fad97d4352a7d12
EzraAmani <ezraamani02@gmail.com>
2026-06-10 19:24:47 +0300

    favorites and add to card  interactivity added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function togglefavorite() {
-    const favoriteicon = document.querySelector('.wishlist-btn .fa-heart');
+function togglefavorite(button) {
+    const favoriteicon = button.querySelector('.fa-heart');
     favoriteicon.classList.toggle('active');
     favoriteicon.classList.toggle('fa-regular');
     favoriteicon.classList.toggle('fa-solid');
 }
 
-function addtocart() {
-    const button = document.querySelector('.add-to-cart-btn');
+function addtocart(button) {
+    // const button = document.querySelector('.add-to-cart-btn');
     button.innerHTML = '<i class="fa-solid fa-check"></i> Added to cart';
 }
