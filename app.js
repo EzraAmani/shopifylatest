bc906ecc6d0978f1557a3f04a56ce54d8807b6b7
EzraAmani <ezraamani02@gmail.com>
2026-06-12 19:35:51 +0300

    sorting assignment done

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ function addtocart(button) {
     // const button = document.querySelector('.add-to-cart-btn');
     button.innerHTML = '<i class="fa-solid fa-check"></i> Added to cart';
 }
+
+
+let priceList = [32, 23, 9, 11, 123];
+
+priceList.sort(function (a, b) {
+    return a - b;
+});
+
+console.log(priceList);
